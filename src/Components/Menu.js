0ced7101fe37d53994f7cd40b1d04acbfef8bf5b
scvkurt03 <kurtgray@gmail.com>
2023-04-13 20:44:11 -0700

    rem http package

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,36 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { menuOptions, formatValue } from "../config/menuOptions";
 import { handler } from "../config/temp.js";
 
 function Menu() {
-  const { formValues, setFormValues } = useState({
-    gradeLevel: "1",
-    subjectArea: "Math",
+  const [formValues, setFormValues] = useState({
+    gradeLevel: "",
+    subjectArea: "",
     unit: "",
-    numLearningObjectives: 2,
+    numLearningObjectives: "",
   });
+  const [response, setResponse] = useState({});
+
+  useEffect(() => {
+    response != {} && console.log(response);
+  }, [response, setResponse]);
 
   return (
     <div>
-      <form onSubmit={handler()}>
-        <label htmlFor="grade-level">Choose a Grade Level:</label>
-        <select name="grade-level" id="grade-level">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const data = await handler(formValues);
+          setResponse(data);
+        }}
+      >
+        <label htmlFor="gradeLevel">Choose a Grade Level:</label>
+        <select
+          name="gradeLevel"
+          id="gradeLevel"
+          value={formValues.gradeLevel}
+          onChange={(e) => {
+            setFormValues({ ...formValues, [e.target.name]: e.target.value });
+          }}
+        >
           {menuOptions["gradeLevel"].map((level, idx) => (
             <option value={level} key={idx}>
               {level}
             </option>
           ))}
         </select>
-        <label htmlFor="subject-area">Choose a Subject Area:</label>
-        <select name="subject-area" id="subject-area">
+        <label htmlFor="subjectArea">Choose a Subject Area:</label>
+        <select
+          name="subjectArea"
+          id="subjectArea"
+          value={formValues.subjectArea}
+          onChange={(e) => {
+            setFormValues({ ...formValues, [e.target.name]: e.target.value });
+          }}
+        >
           {menuOptions["subjectArea"].map((subject, idx) => (
             <option value={formatValue(subject)} key={idx}>
               {subject}
             </option>
           ))}
         </select>
-        <label htmlFor="learning-objectives">Learning Objectives:</label>
-        <select name="learning-objectives" id="learning-objectives">
+        <label htmlFor="numLearningObjectives">Learning Objectives:</label>
+        <select
+          name="numLearningObjectives"
+          id="numLearningObjectives"
+          value={formValues.numLearningObjectives}
+          onChange={(e) => {
+            setFormValues({ ...formValues, [e.target.name]: e.target.value });
+          }}
+        >
           {Array(menuOptions["learningObjectivesMax"])
             .fill("")
             .map((_max, idx) => (
@@ -45,9 +77,14 @@ function Menu() {
           name="unit"
           id="unit"
           placeholder="type unit here"
+          value={formValues.unit}
+          onChange={(e) => {
+            setFormValues({ ...formValues, [e.target.name]: e.target.value });
+          }}
         ></input>
         <input type="submit" value="Submit"></input>
       </form>
+      {response.body && <p>{response.body}</p>}
     </div>
   );
 }
--- src/config/temp.js
+++ src/config/temp.js
@@ -1,11 +1,14 @@
-import https from "https";
 import { SSM } from "@aws-sdk/client-ssm";
 
 const ssm = new SSM({ region: "us-west-1" });
 
 export const handler = async (event, context) => {
   try {
-    const prompt = `Generate a lesson plan for a ${event.gradeLevel}th grade ${event.subjectArea} lesson on ${event.unit}. The lesson should be appropriate for a substitute teacher and should include at least ${event.numLearningObjectives} learning objectives. Please direct me to any activities, assessments, and necessary materials or resources.`;
+    const prompt = `Generate a lesson plan for a ${event.gradeLevel}th grade ${event.subjectArea} 
+    lesson on ${event.unit}. The lesson should be appropriate for a substitute teacher and should 
+    include at least ${event.numLearningObjectives} learning objectives aligned with California 
+    state standards, based on the given grade. Please direct me to any activities, assessments, 
+    and necessary materials or resources.`;
     const apiKey = await getApiKey();
     const response = await generateLessonPlan(prompt, apiKey);
     return {
@@ -23,11 +26,14 @@ export const handler = async (event, context) => {
 
 async function getApiKey() {
   // API key stored at AWS Systems Manager endpoint
-  const ssmParam = await ssm.getParameter({ Name: "/openai/api_key" });
-  return ssmParam.Parameter.Value;
+  // console.log(await ssm.getParameter({ Name: "/openai/api_key" }));
+  // const ssmParam = await ssm.getParameter({ Name: "/openai/api_key" });
+  // return ssmParam.Parameter.Value;
+  return "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";
 }
 
 function generateLessonPlan(prompt, apiKey) {
+  console.log(apiKey);
   const data = JSON.stringify({
     model: "text-davinci-003",
     prompt: prompt,
@@ -37,41 +43,35 @@ function generateLessonPlan(prompt, apiKey) {
   });
 
   const options = {
-    hostname: "api.openai.com",
-    path: "/v1/completions",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`,
-      //   "Content-Length": data.length, //why
     },
+    body: data,
   };
 
   return new Promise((resolve, reject) => {
-    console.log(https);
-    const req = https.request(options, (res) => {
-      let data = "";
-
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      res.on("end", () => {
-        const response = JSON.parse(data);
-        if (response.choices && response.choices.length > 0) {
-          resolve(response);
+    fetch("https://api.openai.com/v1/completions", options)
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.choices && data.choices.length > 0) {
+          console.log(data);
+          resolve(data);
         } else {
+          console.log(data);
           reject("Error generating lesson plan");
         }
+      })
+      .catch((error) => {
+        reject(error);
       });
-    });
-
-    req.on("error", (err) => {
-      reject(err);
-    });
-
-    req.write(data);
-    req.end();
   });
 }
 
